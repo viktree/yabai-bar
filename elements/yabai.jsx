6f233f98de939a993564278c0ef43b8d1363bad0
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2020-08-15 16:42:03 -0400

    fix numbering for two monitor displays

--- elements/yabai.jsx
+++ elements/yabai.jsx
@@ -2,16 +2,15 @@ import { styled } from "uebersicht";
 import theme from "../lib/theme";
 
 const render = ({ total, current, battery }) => (
-  <YabaiSectionBackground battery={battery}>
-    {/* {current} */}
-    {current + " of " + total}
-  </YabaiSectionBackground>
+	<YabaiSectionBackground battery={battery}>
+		{current + " of " + total}
+	</YabaiSectionBackground>
 );
 
 const YabaiSectionBackground = styled("span")(props => ({
-  padding: "0.4% 1%",
-  background: props.battery,
-  color: theme.background
+	padding: "0.4% 1%",
+	background: props.battery,
+	color: theme.background
 }));
 
 export default render;
--- index.jsx
+++ index.jsx
@@ -1,88 +1,106 @@
 import { styled } from "uebersicht";
 
-import theme from "./lib/theme";
-
 import { Time, Spotify, Yabai } from "./elements/index.jsx";
 
-const LocalFontAwsomeHeader = () => (
-  <link
-    rel="stylesheet"
-    type="text/css"
-    href="bar.widget/assets/font-awesome/css/all.min.css"
-  />
-);
+const theme = {
+	margin: "20px",
+	barHeight: "25px",
+
+	background: "#252525",
+	foreground: "#efefef",
+
+	font: "Jetbrains Mono, SF mono, San Serif",
+	fontSize: "11px"
+};
 
 const USE_BASE_TEN = 10;
 
 const computeBatteryColor = data => {
-  const level = parseInt(data, USE_BASE_TEN);
-  // if (level > 90) return "#ADAD00";
-  // if (level > 50) return "#7D837B";
-  if (level > 15) return "#87A0B2";
-  if (level > 8) return "#F3C57C";
-  return "#E57373";
+	const level = parseInt(data, USE_BASE_TEN);
+	if (level > 90) return "#ADAD00";
+	if (level > 50) return "#7D837B";
+	if (level > 15) return "#87A0B2";
+	if (level > 8) return "#F3C57C";
+	return "#E57373";
+};
+
+const parseYabai = yabai => {
+	let { current, total } = yabai.primary;
+	if (yabai.secondary) {
+		total =
+			parseInt(total, USE_BASE_TEN) +
+			parseInt(yabai.secondary.total, USE_BASE_TEN);
+		if (current == "") {
+			current = yabai.secondary.current;
+		}
+	}
+	return { current, total };
 };
 
 const FullBar = ({ output, error }) => {
-  if (error) {
-    console.log(new Date() + " " + error);
-    return <Bar>{error}</Bar>;
-  }
+	if (error) {
+		console.log(new Date() + " " + error);
+		return <Bar>{error}</Bar>;
+	}
 
-  try {
-    const { spotify, battery, time, yabai } = output;
-    const { current, total } = yabai.primary;
+	try {
+		const { spotify, battery, time, yabai } = output;
+		const { current, total } = parseYabai(yabai);
 
-    const batteryColour = computeBatteryColor(battery);
+		const batteryColour = computeBatteryColor(battery);
 
-    return (
-      <Bar>
-        <LocalFontAwsomeHeader />
-        <Yabai battery={batteryColour} total={total} current={current} />
-        <Spotify data={spotify} />
-        <Time battery={batteryColour} isCharging={true} time={time} />
-      </Bar>
-    );
-  } catch (e) {
-    console.log(new Date());
-    console.log("Failed to render one of the bar's subcomponents");
-    console.log(e);
-    return (
-      <Bar>{"Failed to load bar subcomponent bar. See refresh logs."};</Bar>
-    );
-  }
+		return (
+			<Bar>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="bar.widget/assets/font-awesome/css/all.min.css"
+				/>
+				<Yabai battery={batteryColour} total={total} current={current} />
+				<Spotify data={spotify} />
+				<Time battery={batteryColour} isCharging={true} time={time} />
+			</Bar>
+		);
+	} catch (e) {
+		console.log(new Date());
+		console.log("Failed to render one of the bar's subcomponents");
+		console.log(e);
+		return (
+			<Bar>{"Failed to load bar subcomponent bar. See refresh logs."};</Bar>
+		);
+	}
 };
 
 const Bar = styled("div")`
-  position: fixed;
-  top: 10px;
-  right: ${theme.margin};
-  left: ${theme.margin};
+	position: fixed;
+	top: 10px;
+	right: ${theme.margin};
+	left: ${theme.margin};
 
-  background: ${theme.background};
-  color: ${theme.foreground};
+	background: ${theme.background};
+	color: ${theme.foreground};
 
-  height: ${theme.barHeight};
-  line-height: 25px;
+	height: ${theme.barHeight};
+	line-height: 25px;
 
-  font-family: ${theme.font};
-  font-size: ${theme.fontSize};
+	font-family: ${theme.font};
+	font-size: ${theme.fontSize};
 
-  overflow: hidden;
+	overflow: hidden;
 `;
 
 export const command = "sh ./bar.widget/scripts/bar.sh";
-export const refreshFrequency = 1000; // in ms
+export const refreshFrequency = 500; // in ms
 export const initialState = { error: "Initializing bar ..." };
 export const updateState = event => {
-  try {
-    return JSON.parse(event.output);
-  } catch (error) {
-    console.log(new Date());
-    console.log("Output of bar.sh is not valid JSON");
-    console.log("Output from bar.sh:", event);
-    console.log("Output from bar.sh (parsed):", event.output);
-    return { error: "Failed to update bar. See refresh logs." };
-  }
+	try {
+		return JSON.parse(event.output);
+	} catch (error) {
+		console.log(new Date());
+		console.log("Output of bar.sh is not valid JSON");
+		console.log("Output from bar.sh:", event);
+		console.log("Output from bar.sh (parsed):", event.output);
+		return { error: "Failed to update bar. See refresh logs." };
+	}
 };
 export const render = FullBar;
